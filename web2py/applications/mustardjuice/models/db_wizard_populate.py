110163247341f66994b21737e892166569bb1c34
Luther Goh <elfgoh@yahoo.com>
2010-12-12 02:37:23 +0800

    Populate with less data for less complexity. Also stop populating membership contract due to referencing bug, which is currently fixed in trunk

--- web2py/applications/mustardjuice/models/db_wizard_populate.py
+++ web2py/applications/mustardjuice/models/db_wizard_populate.py
@@ -1,6 +1,6 @@
 from gluon.contrib.populate import populate
 if not db(db.auth_user).count():
-     populate(db.auth_user,100)
-     populate(db.t_membership_contract,100)
-     populate(db.t_payment,100)
-     populate(db.t_contract_type,100)
+     populate(db.auth_user,10)
+     #populate(db.t_membership_contract,100)
+     populate(db.t_payment,10)
+     populate(db.t_contract_type,10)
