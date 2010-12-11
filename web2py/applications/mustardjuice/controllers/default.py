413d57fb9e2e29d9ac4d6c9320371b665de31c9b
Luther Goh <elfgoh@yahoo.com>
2010-12-12 04:29:32 +0800

    Added logic and views for prcessing net debit/credit for each member

--- web2py/applications/mustardjuice/controllers/default.py
+++ web2py/applications/mustardjuice/controllers/default.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*- 
 ### required - do no delete
+
+import datetime
+
 def user(): return dict(form=auth())
 def download(): return response.download(request,db)
 def call():
@@ -11,11 +14,33 @@ def call():
 def index():
     #check if user has any contracts
     contract_rows = db(db.t_membership_contract.f_member == auth.user.id).select()
-    
+
     #check if user has any payments
     payment_rows = db(db.t_payment.f_member == auth.user.id).select()
     
-    return dict(user=auth.user.username, contract_rows=contract_rows, payment_rows=payment_rows)
+    total_credit, total_debit, i= 0, 0, 0
+    
+    for row in payment_rows:
+        total_credit += row.f_amount
+    
+    for row in contract_rows:
+        if row.f_end_date == None:
+            end_date = datetime.date.today()
+        
+        #calculating months that have passed in the contract
+        m1 = end_date.month + end_date.year*12
+        m2 = row.f_start_date.month + row.f_start_date.year*12
+        contract_rows[i].months_passed = m1 - m2
+        
+        #calculate sum consumed based on time elapsed for contract
+        contract_rows[i].debit = contract_rows[i].months_passed * contract_rows[i].f_contract_type.f_fee
+        
+        total_debit += contract_rows[i].debit
+        
+        i+=1    
+        
+        
+    return dict(user=auth.user.username, contract_rows=contract_rows, payment_rows=payment_rows, total_debit=total_debit, total_credit=total_credit )
 
 def error():
     return dict()
