4da9a5e9b479d9f500d309fea0276077402cbbaa
Luther Goh <elfgoh@yahoo.com>
2010-12-12 01:15:18 +0800

    Oops forgot to add contract type model

--- web2py/applications/mustardjuice/models/db_wizard.py
+++ web2py/applications/mustardjuice/models/db_wizard.py
@@ -2,6 +2,31 @@
 
 
 ########################################
+db.define_table('t_contract_type',
+    Field('id','id',
+          represent=lambda id:SPAN(id,' ',A('view',_href=URL('contract_type_read',args=id)))),
+    Field('f_name', type='string',
+          label=T('Name')),
+    Field('f_fee', type='integer',
+          label=T('Fee')),
+    Field('active','boolean',default=True,
+          label=T('Active'),writable=False,readable=False),
+    Field('created_on','datetime',default=request.now,
+          label=T('Created On'),writable=False,readable=False),
+    Field('modified_on','datetime',default=request.now,
+          label=T('Modified On'),writable=False,readable=False,
+          update=request.now),
+    Field('created_by',db.auth_user,default=auth.user_id,
+          label=T('Created By'),writable=False,readable=False),
+    Field('modified_by',db.auth_user,default=auth.user_id,
+          label=T('Modified By'),writable=False,readable=False,
+          update=auth.user_id),
+    format='%(f_name)s',
+    migrate=settings.migrate)
+
+db.define_table('t_contract_type_archive',db.t_contract_type,Field('current_record','reference t_contract_type'))
+
+########################################
 db.define_table('t_membership_contract',
     Field('id','id',
           represent=lambda id:SPAN(id,' ',A('view',_href=URL('membership_contract_read',args=id)))),
@@ -11,8 +36,8 @@ db.define_table('t_membership_contract',
           label=T('Start Date')),
     Field('f_end_date', type='date',
           label=T('End Date')),
-    Field('f_contract_type_contract_type', type='string',
-          label=T('Contract Type Contract Type')),
+    Field('f_contract_type', type='reference t_contract_type',
+          label=T('Contract Type')),
     Field('active','boolean',default=True,
           label=T('Active'),writable=False,readable=False),
     Field('created_on','datetime',default=request.now,
