48b842bb1e5a517edfd321bec6362dd94b8a7292
Luther Goh <elfgoh@yahoo.com>
2010-12-11 18:04:00 +0800

    Adding menu items for member crud

--- web2py/applications/mustardjuice/models/menu.py
+++ web2py/applications/mustardjuice/models/menu.py
@@ -6,6 +6,8 @@ response.meta.keywords = settings.keywords
 response.meta.description = settings.description
 response.menu = [
     (T('Index'),URL('index').xml()==URL().xml(),URL('index'),[]),
+    (T('Member Create'),URL('member_create').xml()==URL().xml(),URL('member_create'),[]),
+    (T('Member Select'),URL('member_select').xml()==URL().xml(),URL('member_select'),[]),
     (T('Membership Contract Create'),URL('membership_contract_create').xml()==URL().xml(),URL('membership_contract_create'),[]),
     (T('Membership Contract Select'),URL('membership_contract_select').xml()==URL().xml(),URL('membership_contract_select'),[]),
     (T('Payment Create'),URL('payment_create').xml()==URL().xml(),URL('payment_create'),[]),
