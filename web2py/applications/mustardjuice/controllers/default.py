19773386aab06a7c7e547b7f25712e5b241fd0b0
Luther Goh <elfgoh@yahoo.com>
2010-12-12 01:51:22 +0800

    Added role based access control

--- web2py/applications/mustardjuice/controllers/default.py
+++ web2py/applications/mustardjuice/controllers/default.py
@@ -14,18 +14,18 @@ def index():
 def error():
     return dict()
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def member_create():
     form=crud.create(db.auth_user,next='member_read/[id]')
     return dict(form=form)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def member_read():
     record = db.auth_user(request.args(0)) or redirect(URL('error'))
     form=crud.read(db.auth_user,record)
     return dict(form=form)
  
-@auth.requires_login()
+@auth.requires_membership('admin')
 def member_select():
     f,v=request.args(0),request.args(1)
     try: query=f and db.auth_user[f]==v or db.auth_user
@@ -33,12 +33,12 @@ def member_select():
     rows=db(query)(db.auth_user.id>0).select()
     return dict(rows=rows)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def member_search():
     form, rows=crud.search(db.auth_user)
     return dict(form=form, rows=rows)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def member_update():
     record = db.auth_user(request.args(0)) or redirect(URL('error'))
     form=crud.update(db.auth_user,record,next='member_read/[id]',
@@ -46,18 +46,18 @@ def member_update():
                      )
     return dict(form=form)
    
-@auth.requires_login()
+@auth.requires_membership('admin')
 def membership_contract_create():
     form=crud.create(db.t_membership_contract,next='membership_contract_read/[id]')
     return dict(form=form)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def membership_contract_read():
     record = db.t_membership_contract(request.args(0)) or redirect(URL('error'))
     form=crud.read(db.t_membership_contract,record)
     return dict(form=form)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def membership_contract_update():
     record = db.t_membership_contract(request.args(0),active=True) or redirect(URL('error'))
     form=crud.update(db.t_membership_contract,record,next='membership_contract_read/[id]',
@@ -65,7 +65,7 @@ def membership_contract_update():
                      onaccept=crud.archive)
     return dict(form=form)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def membership_contract_select():
     f,v=request.args(0),request.args(1)
     try: query=f and db.t_membership_contract[f]==v or db.t_membership_contract
@@ -73,23 +73,23 @@ def membership_contract_select():
     rows=db(query)(db.t_membership_contract.active==True).select()
     return dict(rows=rows)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def membership_contract_search():
     form, rows=crud.search(db.t_membership_contract,query=db.t_membership_contract.active==True)
     return dict(form=form, rows=rows)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def payment_create():
     form=crud.create(db.t_payment,next='payment_read/[id]')
     return dict(form=form)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def payment_read():
     record = db.t_payment(request.args(0)) or redirect(URL('error'))
     form=crud.read(db.t_payment,record)
     return dict(form=form)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def payment_update():
     record = db.t_payment(request.args(0),active=True) or redirect(URL('error'))
     form=crud.update(db.t_payment,record,next='payment_read/[id]',
@@ -97,7 +97,7 @@ def payment_update():
                      onaccept=crud.archive)
     return dict(form=form)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def payment_select():
     f,v=request.args(0),request.args(1)
     try: query=f and db.t_payment[f]==v or db.t_payment
@@ -105,23 +105,23 @@ def payment_select():
     rows=db(query)(db.t_payment.active==True).select()
     return dict(rows=rows)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def payment_search():
     form, rows=crud.search(db.t_payment,query=db.t_payment.active==True)
     return dict(form=form, rows=rows)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def contract_type_create():
     form=crud.create(db.t_contract_type,next='contract_type_read/[id]')
     return dict(form=form)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def contract_type_read():
     record = db.t_contract_type(request.args(0)) or redirect(URL('error'))
     form=crud.read(db.t_contract_type,record)
     return dict(form=form)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def contract_type_update():
     record = db.t_contract_type(request.args(0),active=True) or redirect(URL('error'))
     form=crud.update(db.t_contract_type,record,next='contract_type_read/[id]',
@@ -129,7 +129,7 @@ def contract_type_update():
                      onaccept=crud.archive)
     return dict(form=form)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def contract_type_select():
     f,v=request.args(0),request.args(1)
     try: query=f and db.t_contract_type[f]==v or db.t_contract_type
@@ -137,7 +137,7 @@ def contract_type_select():
     rows=db(query)(db.t_contract_type.active==True).select()
     return dict(rows=rows)
 
-@auth.requires_login()
+@auth.requires_membership('admin')
 def contract_type_search():
     form, rows=crud.search(db.t_contract_type,query=db.t_contract_type.active==True)
     return dict(form=form, rows=rows)
